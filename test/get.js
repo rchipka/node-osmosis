dfcaa38951135396a89641027159f2d0387407d1
rc0x03 <chipka01@email.franklin.edu>
2015-10-30 14:07:46 -0500

    Added tests for .get() URL formatting

--- test/get.js
+++ test/get.js
@@ -5,13 +5,42 @@ var fs = require('fs');
 
 var url = server.host+':'+server.port;
 
-module.exports.dynamic_url = function(assert) {
+module.exports.format_url = function(assert) {
     osmosis.get(url)
-    .find('div')
-    .get('/%{test}-${title}-name.html')
+    .data(function(data) {
+        data.name = 'test'
+    })
+    .get('/%{name}-${p}')
+    .then(function(context, data) {
+        assert.ok(context.get('p').text().indexOf('success') !== -1)
+    })
+    .done(function() {
+        assert.done();
+    })
 }
 
-server('/', function(url, req, res, data) {
+module.exports.function_url = function(assert) {
+    osmosis.get(url)
+    .data(function(data) {
+        data.name = 'test'
+    })
+    .get(function(context, data) {
+        return data.name+'-'+context.get('p').content();
+    })
+    .then(function(context, data) {
+        assert.ok(context.get('p').text().indexOf('success') !== -1)
+    })
+    .done(function() {
+        assert.done();
+    })
+}
+
+server('/', function(url, req, res) {
     res.write('<p>test</p><div>'+JSON.stringify(req.query)+'</div>')
+    res.end();
+})
+
+server('/test-test', function(url, req, res) {
+    res.write('<p>success</p>')
     res.end();
 })
