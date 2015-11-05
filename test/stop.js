0bd63ff641df2caeb996e9bd9888f911b823059d
rc0x03 <chipka01@email.franklin.edu>
2015-11-04 20:32:53 -0600

    Fixed tests

--- test/stop.js
+++ test/stop.js
@@ -5,9 +5,10 @@ module.exports.stop = function(assert) {
     osmosis.get('yahoo.com')
     .follow('a:limit(15)')
     .done(function() {
+        assert.ok(true)
         assert.done();
     })
     setTimeout(function() {
         instance.stop()
-    }, 900)
+    }, 300)
 }
